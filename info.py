c2f3620c94512aec2fd44e87b93fdb0e9f224353
TryToLiveAlon_owner <156423343+TryToLiveAlone@users.noreply.github.com>
2024-05-19 09:33:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://brooklyn76596:RXGqxrPL4BB7sSX7@cluster0.tqgjxy0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutoFilter')
 
